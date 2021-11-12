5613ee5fc9d0275dcd210dc89e4c30d9a66ae72c
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-11-11 21:37:41 -0800

    Breaking: Option to choose current system theme

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: './index.js',
+  entry: './index',
   module: {
     rules: [{
       test: /\.lazy\.css$/i,
@@ -13,10 +13,23 @@ module.exports = {
     }, {
       test: /\.tsx?$/,
       loader: 'ts-loader'
+    }, {
+      test: /\.jsx$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react'],
+          plugins: ['react-require']
+        }
+      }
     }]
   },
   resolve: {
-    extensions: ['.js', '.ts']
+    extensions: ['.js', '.ts', '.jsx']
   },
-  plugins: [new HtmlWebpackPlugin({ title: 'Mobx Theme Demo' })]
+  plugins: [new HtmlWebpackPlugin({
+    title: 'Mobx Theme Demo',
+    template: './index.html'
+  })]
 }
--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -1,12 +1,11 @@
 import 'mock-local-storage'
 import { Theme } from '../index'
-import { autorun } from 'mobx'
+import { autorun, runInAction } from 'mobx'
+import { setMedia, matchMedia } from 'mock-match-media'
 
-let dark: boolean
 beforeAll(() => {
-  globalThis.matchMedia = () => ({
-    matches: dark
-  }) as any
+  globalThis.window = globalThis as any
+  globalThis.matchMedia = matchMedia
 })
 beforeEach(() => {
   localStorage.clear()
@@ -14,38 +13,71 @@ beforeEach(() => {
 
 describe('initial value', () => {
   test('dark', () => {
-    dark = true
+    setMedia({
+      'prefers-color-scheme': 'dark'
+    })
     const theme = new Theme()
 
     expect(theme.theme).toBe('dark')
   })
 
   test('light', () => {
-    dark = false
+    setMedia({
+      'prefers-color-scheme': 'light'
+    })
     const theme = new Theme()
 
     expect(theme.theme).toBe('light')
   })
 
   test('reads from localStorage', () => {
-    dark = true
+    setMedia({
+      'prefers-color-scheme': 'dark'
+    })
     localStorage.setItem(Theme.lsKey, 'light')
     expect(new Theme().theme).toBe('light')
   })
 })
 
 test('updates localStorage', () => {
-  dark = false
-  new Theme().theme = 'light'
+  setMedia({
+    'prefers-color-scheme': 'light'
+  })
+  new Theme().selectedTheme = 'light'
   expect(localStorage.getItem(Theme.lsKey)).toBe(JSON.stringify('light'))
 })
 
 test('reactive', () => {
   const log: string[] = []
-  dark = true
+  setMedia({
+    'prefers-color-scheme': 'dark'
+  })
   const theme = new Theme()
   autorun(() => log.push(theme.theme))
   expect(log).toEqual(['dark'])
-  theme.theme = 'light'
+  runInAction(() => {
+    theme.selectedTheme = 'light'
+  })
   expect(log).toEqual(['dark', 'light'])
+  runInAction(() => {
+    theme.selectedTheme = undefined
+  })
+  expect(log).toEqual(['dark', 'light', 'dark'])
+})
+
+test('updates on mediaQuery change', () => {
+  const log: string[] = []
+  const expectedLog: string[] = []
+  setMedia({
+    'prefers-color-scheme': 'dark'
+  })
+  const theme = new Theme()
+  autorun(() => log.push(theme.theme))
+  expectedLog.push('dark')
+  expect(log).toEqual(expectedLog)
+  setMedia({
+    'prefers-color-scheme': 'light'
+  })
+  expectedLog.push('light')
+  expect(log).toEqual(expectedLog)
 })
